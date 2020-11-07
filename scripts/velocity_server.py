b6c0ee72a79c799b669e118a25dc2fddbea83ba0
n-shintaro <shintaro0311@keio.jp>
2020-11-07 22:00:15 +0900

    20201107 fix CMakefile

--- scripts/velocity_server.py
+++ scripts/velocity_server.py
@@ -2,19 +2,20 @@
 
 import rospy
 from std_msgs.msg import String
+from robot_control.srv import RandomPos
 import random
 
-def generate_random(req,res):
-    print("Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b)))
-    res.pos_x=random.uniform(req.min, req.max)
-    res.pos_y=random.uniform(req.min, req.max)
-    return AddTwoIntsResponse(req.a + req.b)
+# def generate_random(req,res):
+#     print("Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b)))
+#     res.pos_x=random.uniform(req.min, req.max)
+#     res.pos_y=random.uniform(req.min, req.max)
+#     return AddTwoIntsResponse(req.a + req.b)
 
-def add_velocity_server():
-    rospy.init_node('velocity_server')
-    s = rospy.Service('/random_target', RandomPos, generate_random)
-    print("Ready to add two ints.")
-    rospy.spin()
+# def add_velocity_server():
+#     rospy.init_node('velocity_server')
+#     s = rospy.Service('/random_target', RandomPos, generate_random)
+#     print("Ready to add two ints.")
+#     rospy.spin()
 
-if __name__ == "__main__":
-    add_velocity_server()
+# if __name__ == "__main__":
+#     add_velocity_server()
